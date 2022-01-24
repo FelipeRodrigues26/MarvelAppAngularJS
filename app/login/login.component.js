6f211ecb11c77aa7110657ab5c0f9179d1d5b463
Felipe Rodrigues <felipemrodrigues26@gmail.com>
2022-01-23 22:47:00 -0300

    inictial 2

--- app/login/login.component.js
+++ app/login/login.component.js
@@ -8,29 +8,34 @@ angular.module('marvelApp')
 function LoginController(AutenticationService, $location){
   this.autenticationService = AutenticationService
   this.user = new User("felipemrodrigues26@gmail.com", "123456");
+  this.alertVisible = false;
  
-  this.signIn =  function() {  
-    this.autenticationService.signIn(this.user.email, this.user.pass)
-      .then((result) => {
+  this.signIn =  async function() {  
+    response = await this.autenticationService.signIn(this.user.email, this.user.pass)
+     
         this.alertVisible = true;
         this.msgSucessError = 'Autenticado com sucesso.'; 
         this.alertType = 'alert-success';
-        console.log('teste'+this.msgSucessError)
-       
-        // setTimeout(function(){
-        //   $location.path('/personagem/lista');
+        console.log(this.msgSucessError)
+        console.log(this.alertType);
+       // setTimeout(function(){
+        // $location.path('/personagem/lista');
         //   window.location.reload();
-        // }, 6000)
-      }) 
-      .catch( (error)  => {
+        //}, 4000)
+        console.log('entrou')
+     
+        console.log('error')
         this.alertVisible = true;
         this.alertType = 'alert-danger';
         this.msgSucessError = 'Autenticação falhou.';
-         console.log("Erro ao logar" + error)
-      });
+        // console.log("Erro ao logar" + error)
+         
+      
 
+      console.log("dsdss"+response)
+      return response
   }
-
+  
   this.signOut = function(){
     this.autenticationService.signOut();
   }

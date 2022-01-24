d2792b1e227bb376f49c4082d98ee7f58271a99d
Felipe Rodrigues <felipemrodrigues26@gmail.com>
2022-01-24 01:53:15 -0300

    inictial 26

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static('./app'));
 
-app.get('../', function (req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
-});
-app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'app/'}),
+);
+
 app.listen(process.env.PORT || 3000);
